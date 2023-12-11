1583d89e8e07168e87269bbdc0389dc362475b05
Lux Barker <29106316+k-nox@users.noreply.github.com>
2023-12-11 15:09:37 -0800

    spin up GraphQL server on startup

--- main.go
+++ main.go
@@ -3,16 +3,25 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/k-nox/ddb-backend-developer-challenge/graph"
+	"github.com/k-nox/ddb-backend-developer-challenge/graph/generated"
 	_ "github.com/mattn/go-sqlite3"
 	migrate "github.com/rubenv/sql-migrate"
 	"log"
+	"net/http"
+	"os"
 )
+
+const defaultPort = "8080"
 
 func main() {
 	migrations := &migrate.FileMigrationSource{
 		Dir: "db/migrations",
 	}
 
+	// start up database connection
 	db, err := sql.Open("sqlite3", "db/data.db")
 	if err != nil {
 		log.Fatalf("error opening sqlite3: %s\n", err.Error())
@@ -24,9 +33,22 @@ func main() {
 		}
 	}(db)
 
+	// apply any new migrations
 	n, err := migrate.Exec(db, "sqlite3", migrations, migrate.Up)
 	if err != nil {
 		log.Fatalf("error applying migrations: %s", err.Error())
 	}
-	fmt.Printf("Applied %d migrations!\n", n)
+	log.Printf("Applied %d migrations!\n", n)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	http.Handle("/", playground.Handler("GraphQL Playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL Playground", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
